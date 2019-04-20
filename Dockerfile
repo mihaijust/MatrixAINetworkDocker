7a221a61684e5862ac3188fa8ad3ece1a0e05635
disarmm <leonardsjunk@gmail.com>
2019-04-20 01:24:40 -0500

    updated

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Build Gman in a stock Go builder container #shang and yang
 FROM golang:1.12-alpine 
 
-RUN apk add --no-cache make gcc musl-dev linux-headers git
+RUN apk add --no-cache make gcc musl-dev linux-headers git gnupg
 
 ADD . /go-matrix
 RUN cd /go-matrix && make gman
@@ -9,7 +9,7 @@ RUN cd /go-matrix && make gman
 # Pull Gman into a second stage deploy alpine container
 
 RUN apk add --no-cache ca-certificates
-RUN  ln -s  /go-matrix/nodeStartup.sh /usr/local/bin/nodeStart
+RUN  ln -s  /go-matrix/nodeConfig.sh /usr/local/bin/nodeConfig
 
 EXPOSE 8341 8546 50505 50505/udp 30304/udp
-CMD ["nodeStart"]
+ENTRYPOINT ["nodeConfig"]
---
+++ nodeConfig.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -f "/go-matrix/man.json" ]; then
+  mv /go-matrix/man.json /go-matrix/chaindata/
+fi
+
+PASS="$(date +%s | sha256sum | base64 | head -c 8)1@"
+echo $PASS > /go-matrix/gman.pass
+echo $PASS >> /go-matrix/gman.pass
+cat /go-matrix/gman.pass | /go-matrix/build/bin/gman --datadir /go-matrix/chaindata aes --aesin /go-matrix/chaindata/signAccount.json --aesout /go-matrix-/entrust.json
+
+MANWALLET="$(ls /go-matrix/chaindata/keystore/)"
+
+if [ ! -d "/go-matrix/chaindata/gman" ]; then
+  cd /go-matrix && ./gman --datadir /go-matrix/chaindata init MANGenesis.json
+fi
+
+cd /go-matrix/build/bin && cat /go-matrix/gman.pass | ./gman --datadir /go-matrix/chaindata --networkid 1 --debug --verbosity 5 --manAddress $MANWALLET --entrust /go-matrix/entrust.json --gcmode archive --outputinfo 1 --syncmode full
