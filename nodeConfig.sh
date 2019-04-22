8853ac6723f465360be47b18c591a393f2d61764
disarmm <49657913+disarmm@users.noreply.github.com>
2019-04-21 22:45:11 -0500

    Update nodeConfig.sh

--- nodeConfig.sh
+++ nodeConfig.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+#This is used to detect first time run and moves a file
 if [ -f "/go-matrix/man.json" ]; then
   mv /go-matrix/man.json /go-matrix/chaindata/
 fi
 
 #This section creates your entrust.json file on the fly each time you start the container
 #This requires that you mount a persistent volume from your host OS to the /go-matrix/chaindata directory
+#You must have a signAccount.json file in your mounted directory
 PASS="$(date +%s | sha256sum | base64 | head -c 4)pO1@"
 echo $PASS > /go-matrix/gman.pass
 echo $PASS >> /go-matrix/gman.pass
@@ -13,6 +15,7 @@ cat /go-matrix/gman.pass | /go-matrix/build/bin/gman --datadir /go-matrix/chaind
 
 MANWALLET="$(ls /go-matrix/chaindata/keystore/)"
 
+#This also detects if this is a first run and intializes the genisis block
 if [ ! -d "/go-matrix/chaindata/gman" ]; then
   cd /go-matrix/build/bin && ./gman --datadir /go-matrix/chaindata init /go-matrix/MANGenesis.json
 fi
